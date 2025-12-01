40f52a7479ff4c9feb526f139c4b268f7f8de714
Devanshu Chandu Mankar <158685271+devanshumankar@users.noreply.github.com>
2025-12-01 16:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const existing = document.querySelector("#existing")
 const submitBtn = document.querySelector("#submit")
-	        if (localStorage.length > 0) {
-	            existing.style.display = "inline"
-	        } else {
-
-            existing.style.display = "none"
-        }
-        submitBtn.addEventListener("click", () => {
-            const userName = document.querySelector("#username")
-            const password = document.querySelector("#password")
-            const exists = document.querySelector("#checkbox")
-            if (exists.checked) {
-                localStorage.setItem(userName.value, password.value)
-            }
-            alert(`Logged in as ${userName.value}`)
-        })
+if (localStorage.length > 0) {
+	existing.style.display = "inline"
+} else{
+	existing.style.display = "none"
+}
+submitBtn.addEventListener("click", () => {
+	const userName = document.querySelector("#username")
+	const password = document.querySelector("#password")
+	const exists = document.querySelector("#checkbox")
+	if (exists.checked) {
+        localStorage.setItem(userName.value, password.value)
+	}
+    alert(`Logged in as ${userName.value}`)
+})
